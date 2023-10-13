d0e519beb6ac8736e67c26cda3fe9413bdb89c7c
Lei Liu <65260437+JeremyLeiLiu@users.noreply.github.com>
2023-10-12 22:46:56 -0400

    The file 'download_mt5_base.py' has been uploaded.

---
+++ download_mt5_base.py
@@ -0,0 +1,17 @@
+"""
+File: download_mt5_base.py
+Author: Lei Liu
+Date: Nov 22, 2022
+
+Download the mT5-base model from HuggingFace Transformers to local directory.
+"""
+
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+
+# Load mT5 tokenizer and mT5-base model
+tokenizer = AutoTokenizer.from_pretrained("google/mt5-base")
+model = AutoModelForSeq2SeqLM.from_pretrained("google/mt5-base")
+
+# Save both the tokenizer and model
+tokenizer.save_pretrained("mt5-base")
+model.save_pretrained("mt5-base")
